c885f1118f2cd5dfbe3dfdb81f2d65cc2ab1d3fe
jung1240 <sinue6308@naver.com>
2020-11-10 12:11:16 +0900

    2020/11/10 ch01 연습예제

---
+++ ch01/src/sub1/Helloworld.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/*
+ * 날짜 : 2020/11/02
+ * 이름 : 정유락
+ * 내용 : Ch01 자바 Hello World 출력하기
+ */
+public class Helloworld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		
+	}
+}
