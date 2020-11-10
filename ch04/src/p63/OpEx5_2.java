15e3af07b8560d4c6c8d2bac429773994bd33f35
jung1240 <sinue6308@naver.com>
2020-11-10 11:58:01 +0900

    2020/11/10 ch04연습예제

---
+++ ch04/src/p63/OpEx5_2.java
@@ -0,0 +1,18 @@
+package p63;
+
+public class OpEx5_2 {
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=5;
+		
+		System.out.println(a==b & test());
+		System.out.println(a==b && test());
+		
+		}
+	public static boolean test() {
+		System.out.println("test()메서드 실행됨");
+		return true;
+	}
+
+}
---
+++ ch04/src/p64/OpEx5_3.java
@@ -0,0 +1,12 @@
+package p64;
+
+public class OpEx5_3 {
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=0;
+		
+		System.out.println(b>0&(a/b>0));
+	}
+
+}
