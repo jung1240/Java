86a96119370f54a855572f3962c5d3a321b87001
jung1240 <sinue6308@naver.com>
2020-11-10 11:52:46 +0900

    2020/11/10 ch02 연습문제

---
+++ ch02/src/p32/HelloJava.java
@@ -0,0 +1,20 @@
+package p32;
+
+/**
+ * 날짜 : 2020/11/02
+ * 이름 : 정유락
+ * 내용 : /ch02. 자바설치 교제 p031
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+		/*
+		 * Hello Java를 출력하는 실행문을 주석처리 하고
+		 * 아해 안녕 자바라고 출력되는 실행문을 추가
+		 */
+		
+		//"안녕 자바"라고 출력되는 실행문
+		System.out.println("안녕 자바");
+	}
+}
---
+++ ch02/src/sub1/HelloJava.java
@@ -0,0 +1,13 @@
+package sub1;
+
+/*
+ * 날짜 : 2020/11/02
+ * 이름 : 정유락
+ * 내용 : /ch02. 자바설치 교제 p031
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+	}
+}
