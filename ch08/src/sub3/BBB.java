10b2aac559799dce7e29bbcfc36fa030f8a57aa7
jung1240 <sinue6308@naver.com>
2020-11-11 10:12:35 +0900

    2020/11/11 ch08 sub3 수업예제 추가

---
+++ ch08/src/sub3/BBB.java
@@ -0,0 +1,17 @@
+package sub3;
+
+public class BBB extends AAA{
+
+	@Override
+	public final void method1() {
+		System.out.println("BBB - method1...");
+	}
+	
+	public void method2(int a) {
+		System.out.println("BBB - method2...");
+	}
+	
+	public void method3() {
+		System.out.println("BBB - method3...");
+	}
+}
