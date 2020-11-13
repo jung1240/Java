5b3f1fe2a8caea23fa2f313bcdda5ca7d6f92cfb
jung1240 <sinue6308@naver.com>
2020-11-14 08:29:47 +0900

    2020/11/14 test2 자바 테스트문제

---
+++ Test2/src/test03/JavaTest3.java
@@ -0,0 +1,24 @@
+package test03;
+
+public class JavaTest3 {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+		System.out.print("\n");
+		
+		for(int j=0; j<5; j++) {
+			int temp = arr[j];
+			arr[j] = arr[9-j];
+			arr[9-j] = temp;
+		}
+		
+		for(int k=0; k<10; k++) {
+			System.out.print(arr[k]+", ");
+		}
+	}
+}
